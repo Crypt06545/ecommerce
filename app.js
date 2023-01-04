f9d4ebfd718cf147e01c36ed3e418a9d63861d43
Crypt06545 <mehedihasansagor2005@gmail.com>
2023-01-04 10:00:52 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+
+
+app.use('/api', require('./routes/authRouter'))
+
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}).then(con => {
+    console.log(`MongoDB Database connected`)
+})
+
+
+
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+    console.log(`server is running on port`, port);
+})
